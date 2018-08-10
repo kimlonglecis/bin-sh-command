0487d3708169f705495b1e1a08bc29383dfef2af
Docker Lamp 16.04 <kimlongyakura@Yahoo.com>
2018-08-10 12:11:57 -0700

    Minor Changes

--- git_add_push_master_comment.sh
+++ git_add_push_master_comment.sh
@@ -8,13 +8,13 @@ git add .
 echo ""
 echo ""
 echo "GIT COMMIT"
-read -p "ENTER COMMIT COMMENT: " comment
+read -p "ENTER COMMIT COMMENT (Default: Minor Changes): " comment
 comment=${comment:-Minor Changes}
 git commit -m "$comment"
 
 echo ""
 echo ""
 echo "GIT PUSH"
-read -p "ENTER BRANCH NAME: " name
+read -p "ENTER BRANCH NAME (Default: master): " name
 name=${name:-master}
 git push origin $name
