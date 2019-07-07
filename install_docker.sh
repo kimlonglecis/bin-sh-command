f24ecc0d25d76d02e0d98129f7e378c258913c15
long le <kimlonglecis@gmail.com>
2019-07-06 17:47:59 -0700

    minor changes

---
+++ install_docker.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
+
+sudo apt update
+
+apt-cache policy docker-ce -y
+
+sudo apt install docker-ce -y
+
+sudo usermod -aG docker ${USER}
+
+# Install Docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+
+su - ${USER}
+
---
+++ install_node_npm_gulp.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# Install Nodejs
+curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+sudo apt install nodejs -y
+
+# Install Ruby
+sudo apt install ruby-full ruby-dev -y
+
+# Install Compass
+sudo apt install build-essential -y
+sudo gem install compass
+
+# Fix Gulp watch error out of memory
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+
