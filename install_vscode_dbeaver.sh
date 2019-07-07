8e8aba80521f7a3bafeea0348df953f3e81572a7
long le <kimlonglecis@gmail.com>
2019-07-06 18:07:14 -0700

    minor changes

--- install_vscode_dbeaver.sh
+++ install_vscode_dbeaver.sh
@@ -12,5 +12,14 @@ sudo apt update
 sudo apt install code -y
 
 
+# Install DBEAVER
+wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | sudo apt-key add -
+
+echo "deb https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
+
+sudo apt update
+    
+sudo apt -y  install dbeaver-ce
+
 
 
