3029dd4fd887d1da4c765de8552e056e3f493eea
long le <kimlonglecis@gmail.com>
2020-08-26 12:09:41 -0700

    minor changes

--- .bash_aliases
+++ .bash_aliases
@@ -3,3 +3,4 @@ export PATH=$PATH:$HOME/bin
 alias wwww="cd /www"
 alias bin="cd ~/bin"
 alias www="cd ~/Desktop/www"
+alias sc="scrapy"
--- install_scrapy.sh
+++ install_scrapy.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-sudo apt-get install python3 python3-dev python3-pip libxml2-dev libxslt1-dev zlib1g-dev libffi-dev libssl-dev
+sudo apt-get install python3 python3-dev python3-pip libxml2-dev libxslt1-dev zlib1g-dev libffi-dev libssl-dev -y
 
 sudo apt update
 
-pip3 install scrapy
+pip3 install scrapy pandas
 
 
 
