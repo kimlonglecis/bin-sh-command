ef6fff06820bf14843568b7f8e48a9d5663ae6a7
long le <kimlonglecis@gmail.com>
2020-10-26 23:49:09 -0700

    fixing install_composer.sh

--- install_composer.sh
+++ install_composer.sh
@@ -2,6 +2,10 @@
 
 sudo apt install curl php-cli php-mbstring git unzip -y
 
-curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer ##Composer ready
+#curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer ##Composer ready
+
+curl -sS https://raw.githubusercontent.com/composer/getcomposer.org/76a7060ccb93902cd7576b67264ad91c8a2700e2/web/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer ##Composer ready
+
+
 
 
