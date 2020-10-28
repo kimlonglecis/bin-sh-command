51d86573c54c9c54556778fc3f4ecdc4d522267c
Long <kimlongyakura@yahoo.com>
2020-10-28 16:58:57 -0700

    composer v1

--- install_composer_v1.sh
+++ install_composer_v1.sh
@@ -4,7 +4,7 @@ sudo apt install curl php-cli php-mbstring git unzip -y
 
 #curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer ##Composer ready
 
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer --version=1.10.16 
+sudo curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer --version=1.10.16 
 
 
 
