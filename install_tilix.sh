0befcb7f8302ba243cbe1b91176972d360f62313
long le <kimlonglecis@gmail.com>
2021-03-18 12:10:54 -0700

    minor changes

--- install_tilix.sh
+++ install_tilix.sh
@@ -4,3 +4,7 @@
 sudo apt-get update
 sudo apt-get install -y tilix
 
+
+mkdir -p ~/.config/tilix/schemes
+wget -qO $HOME"/.config/tilix/schemes/spacegray.json" https://git.io/v7QV3
+wget -qO $HOME"/.config/tilix/schemes/spacegray-eighties.json" https://git.io/v7QVB
