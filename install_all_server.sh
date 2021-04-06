4053e86eee4002fc74b3a8ed0305c17ddc50552a
long le <kimlonglecis@gmail.com>
2021-04-05 23:31:43 -0700

    install_all_server.sh

--- install_all_server.sh
+++ install_all_server.sh
@@ -11,10 +11,9 @@ install_composer_v1.sh
 install_docker_compose.sh
 
 # Increasing vm.max_map_couint = 262144 to keep Elasticsearch Node on
-echo ""
-echo "Added by Kim Long Le: Brandcreative LLC" >> /etc/sysctl.conf
-echo "fs.inotify.max_user_watches=524288" >> /etc/sysctl.conf
-echo "vm.max_map_count=262144" >> /etc/sysctl.conf
+sudo sysctl -w fs.inotify.max_user_watches=524288
+sudo sysctl -w vm.max_map_count=262144
+
 
 
 
