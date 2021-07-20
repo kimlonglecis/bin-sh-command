58e75fe07a5be7fafed78b0064a0b47065c06510
long le <kimlonglecis@gmail.com>
2021-07-20 12:40:31 -0700

    minor changes

--- install_s3cmd.sh
+++ install_s3cmd.sh
@@ -6,7 +6,7 @@ sudo apt-get install python-setuptools
 
 wget -q -O- "https://github.com/s3tools/s3cmd/releases/download/v2.1.0/s3cmd-2.1.0.tar.gz" | tar -xz -C ~
 
-sudo python ~/s3cmd-2.1.0/setup.py install
+sudo python3 ~/s3cmd-2.1.0/setup.py install
 
 sudo rm -rf ~/s3cmd-*/
 
