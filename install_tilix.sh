030bd5c26abc24fc062a23de75b25a2f7aed3aa1
long le <kimlonglecis@gmail.com>
2022-11-22 12:08:18 -0800

    minor changes

--- install_tilix.sh
+++ install_tilix.sh
@@ -8,3 +8,7 @@ sudo apt-get install -y tilix
 mkdir -p ~/.config/tilix/schemes
 wget -qO $HOME"/.config/tilix/schemes/spacegray.json" https://git.io/v7QV3
 wget -qO $HOME"/.config/tilix/schemes/spacegray-eighties.json" https://git.io/v7QVB
+
+
+# Set default terminal
+sudo update-alternatives --config x-terminal-emulator
